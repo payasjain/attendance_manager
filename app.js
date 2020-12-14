088a3959ad1b0fdf07b72a32da9648f890ce3238
payasjain <payasjain21@gmail.com>
2020-12-14 23:21:34 +0530

    config.env created

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // Including Packages!
 var express = require("express"),
   app = express(),
+  dotenv = require("dotenv"),
   mongoose = require("mongoose"),
   methodOverride = require('method-override'),
   bodyParser = require("body-parser");
@@ -14,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(methodOverride("_method"));
 app.use(express.static(__dirname + "/public"));
-
+dotenv.config({ path: "./config.env" });
 //Connection To Database
 mongoose.connect(
   "mongodb+srv://payasjain:Payasjain@21@attendance-9wmmr.mongodb.net/test",
@@ -33,6 +34,6 @@ var Team = require("./models/team.js");
 
 app.use("/", routes);
 
-app.listen(27017, function() {
+app.listen(process.env.PORT||27017, function() {
   console.log("Server Started!");
 });
