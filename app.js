b5aa459f8d7b7ccfc32b2d7a900d61749082d0d9
payasjain <payasjain21@gmail.com>
2020-12-14 23:02:25 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+// Including Packages!
+var express = require("express"),
+  app = express(),
+  mongoose = require("mongoose"),
+  methodOverride = require('method-override'),
+  bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Including Routes!
+var routes = require("./routes/routes.js");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+app.use(methodOverride("_method"));
+app.use(express.static(__dirname + "/public"));
+
+//Connection To Database
+mongoose.connect(
+  "mongodb+srv://payasjain:Payasjain@21@attendance-9wmmr.mongodb.net/test",
+  { useNewUrlParser: true, useUnifiedTopology : true },
+  (error) => {
+    if (!error) {
+      console.log("Connection to db successful");
+    } else {
+      console.log(error);
+    }
+  }
+);
+
+//Including Model
+var Team = require("./models/team.js");
+
+app.use("/", routes);
+
+app.listen(27017, function() {
+  console.log("Server Started!");
+});
